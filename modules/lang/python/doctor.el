0ae1bc9b68454f4d4a319fae9d301db9db35270e
sadsock <mantis-shrimp@outlook.com>
2023-02-03 10:34:45 +0800

    tweak(default): removed the annoying package in python layer
    Everything except lsp has been removed.

--- modules/lang/python/doctor.el
+++ modules/lang/python/doctor.el
@@ -16,42 +16,43 @@
                 (zerop (shell-command "python3 -c 'import setuptools'")))
       (warn! "setuptools wasn't detected, which anaconda-mode requires"))))
 
-(when (modulep! +pyenv)
-  (if (not (executable-find "pyenv"))
-      (warn! "Couldn't find pyenv in your PATH")
-    (unless (split-string (shell-command-to-string "pyenv versions --bare") "\n" t)
-      (warn! "No versions of python are available via pyenv, did you forget to install one?"))))
+;;(when (modulep! +pyenv)
+;;  (if (not (executable-find "pyenv"))
+;;      (warn! "Couldn't find pyenv in your PATH")
+;;    (unless (split-string (shell-command-to-string "pyenv versions --bare") "\n" t)
+;;      (warn! "No versions of python are available via pyenv, did you forget to install one?"))))
 
-(when (modulep! +conda)
-  (unless (executable-find "conda")
-    (warn! "Couldn't find conda in your PATH")))
+;;(when (modulep! +conda)
+;;  (unless (executable-find "conda")
+;;    (warn! "Couldn't find conda in your PATH")))
 
-(when (modulep! +poetry)
-  (if (not (executable-find "poetry"))
-      (warn! "Couldn't find poetry in your PATH")))
+;;(when (modulep! +poetry)
+;;  (if (not (executable-find "poetry"))
+;;      (warn! "Couldn't find poetry in your PATH")))
 
-(when (modulep! +cython)
-  (unless (executable-find "cython")
-    (warn! "Couldn't find cython. cython-mode will not work.")))
+;;(when (modulep! +cython)
+;;  (unless (executable-find "cython")
+;;    (warn! "Couldn't find cython. cython-mode will not work.")))
 
-(when (modulep! +ipython)
-  (unless (executable-find "ipython")
-    (warn! "Couldn't find ipython in your PATH")))
+;;(when (modulep! +ipython)
+;;  (unless (executable-find "ipython")
+;;    (warn! "Couldn't find ipython in your PATH")))
 
-(unless (executable-find "pytest")
-  (warn! "Couldn't find pytest. Running tests through pytest will not work."))
-
-(unless (executable-find "nosetests")
-  (warn! "Couldn't find nosetests. Running tests through nose will not work."))
-
-(unless (executable-find "pipenv")
-  (warn! "Couldn't find pipenv. pipenv support will not work."))
-
-(unless (executable-find "isort")
-  (warn! "Couldn't find isort. Import sorting will not work."))
-
-(when (modulep! :editor format)
-  (unless (executable-find "pyflakes")
-    (warn! "Couldn't find pyflakes. Import management will not work."))
-  (unless (executable-find "black")
-    (warn! "Couldn't find black. Code formatting will not work.")))
+;;(unless (executable-find "pytest")
+;;  (warn! "Couldn't find pytest. Running tests through pytest will not work."))
+;;
+;;(unless (executable-find "nosetests")
+;;  (warn! "Couldn't find nosetests. Running tests through nose will not work."))
+;;
+;;(unless (executable-find "pipenv")
+;;  (warn! "Couldn't find pipenv. pipenv support will not work."))
+;;
+;;(unless (executable-find "isort")
+;;  (warn! "Couldn't find isort. Import sorting will not work."))
+;;
+;;(when (modulep! :editor format)
+;;  (unless (executable-find "pyflakes")
+;;    (warn! "Couldn't find pyflakes. Import management will not work."))
+;;  (unless (executable-find "black")
+;;    (warn! "Couldn't find black. Code formatting will not work.")))
+;;
